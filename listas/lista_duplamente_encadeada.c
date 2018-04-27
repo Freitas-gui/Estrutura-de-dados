6f9199b57c142963342fc0d57a6627d80ae16f8a
Freitas-gui <freitas-gui19@hotmail.com>
2018-04-27 03:29:08 -0300

    Adicionei a função : *remocao_chave_dada

--- listas/lista_duplamente_encadeada.c
+++ listas/lista_duplamente_encadeada.c
@@ -98,5 +98,52 @@ no *insere_ordenado(no *L,no *novo){
         aux->ant=novo;
     }
     return L;
-
 }
+no *remocao_chave_dada(no *L,int chave){
+    //declaracao das variaveis
+    no*aux=L;
+    //caso: Lista vazia
+    if(L==NULL)
+        return L;
+    //laço de repetição para,pecorrer a lista até o final{
+    while(aux!=NULL){
+        //caso seja o primeiro: aux->ant==NULL;
+        if(aux->ant==NULL){
+            L=aux->prox;
+            aux->prox->ant=NULL;
+            free(aux);
+        }
+        //caso seja o ultimo: aux->prox==NULL;
+        else if(aux->prox==NULL){
+            aux->ant->prox=NULL;
+            free(aux);
+        }
+        //caso seja no meio: defalt}
+        else{
+            aux->prox->ant=aux->ant;
+            aux->ant->prox=aux->prox;
+            free(aux);
+        }
+    //retornar
+    }
+    return L;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
